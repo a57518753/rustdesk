d81b31157b7765ee5e9467b2092095ba61ce29df
a57518753 <251646530@qq.com>
2025-09-18 09:28:08 +0800

    修改替换 API 地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://120.26.114.137:21114".to_owned()
 }
 
 #[inline]
